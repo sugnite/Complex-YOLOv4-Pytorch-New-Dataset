f0798cd32d5f4c264a7d1a0032ba67f8ae6aafc3
sugnite <af2014@hw.ac.uk>
2022-08-10 19:54:23 +0100

    Cleaned the new 3D object to fit the new type of data

--- src/data_process/ply_data_utils.py
+++ src/data_process/ply_data_utils.py
@@ -16,7 +16,7 @@ import math
 class Object3d(object):
     ''' 3d object label '''
 
-    def __init__(self, label_file_line, labels_list=[], multiplied_ratio=1):
+    def __init__(self, label_file_line, labels_list=[], pcd_ratio=[1,0,0,0]):
         data = label_file_line.split(' ')
         data[1:] = np.array([float(x) for x in data[1:]]) 
         # This is the froat of the labels file
@@ -25,34 +25,26 @@ class Object3d(object):
         # extract label, boudning box, orientation
         self.type = data[0]  # 'door', 'strairs', ...
         self.cls_id = self.cls_type_to_id(self.type, labels_list)
-        self.truncation = 0  # truncated pixel ratio [0..1]                                 # USeless
-        self.occlusion = 0  # 0=visible, 1=partly occluded, 2=fully occluded, 3=unknown     # Useless
-        # self.alpha = 0  # object observation angle [-pi..pi]   was data3                    # ??????        
+        self.truncation = 0  # truncated pixel ratio [0..1]                                 # Useless in our dataset
+        self.occlusion = 0  # 0=visible, 1=partly occluded, 2=fully occluded, 3=unknown     # Useless in our dataset   
 
         # extract 2d bounding box in 0-based coordinates
-        # self.xmin = data[4]  # left                                                         # ??????
-        # self.ymin = data[5]  # top                                                          # ??????
-        # self.xmax = data[6]  # right                                                        # ??????
-        # self.ymax = data[7]  # bottom                                                       # ??????
-        # self.box2d = np.array([self.xmin, self.ymin, self.xmax, self.ymax])                 # Combination of before
-
+        # get all the ratios
+        multiplied_ratio, x_off, y_off, z_off = pcd_ratio
         # extract 3d bounding box information
-        self.x = data[1] * multiplied_ratio
-        self.y = data[2] * multiplied_ratio
-        self.z = data[3] * multiplied_ratio
+        self.x = data[1] * multiplied_ratio + x_off
+        self.y = data[2] * multiplied_ratio + y_off
+        self.z = data[3] + z_off
+
         self.rx = math.radians(data[7] / math.pi)
         self.ry = math.radians(data[8] / math.pi)
-        # self.rz = math.radians(data[9] / math.pi)
         self.rz = data[9] * (math.pi/180)
-        self.h = data[6] * multiplied_ratio # box height                                                      # OK
-        self.w = data[5] * multiplied_ratio # box width                                                       # OK
-        self.l = data[4] * multiplied_ratio # box length (in meters)                                          # OK bu not in meters ??
-        # self.t = (data[11], data[12], data[13])  # location (x,y,z) in camera coord.        # ??????                                                     
-        # self.dis_to_cam = np.linalg.norm(self.t)                                            # Useless -- ????
-        # self.ry = data[14]  # yaw angle (around Y-axis in camera coordinates) [-pi..pi]
-        self.score = -1.0                                                                   # Useless -- ????
-        self.level_str = 'Easy'                                                             # Useless                 OK
-        self.level =  1                                                                     # Useless                 OK
+        self.h = data[6]                     # box height                          
+        self.w = data[5] * multiplied_ratio # box width                            
+        self.l = data[4] * multiplied_ratio # box length (in meters)               
+        self.score = -1.0                                                                   
+        self.level_str = 'Easy'                                                             
+        self.level =  1                                                                     
         # label list of shape x, y, z, h, w, l, ry
         # create new rz
         self.new_rz =  -self.ry - np.pi / 2
@@ -64,7 +56,7 @@ class Object3d(object):
             return -1
         return labels_list.index(cls_type)
     
-def read_label(label_filename, labels_list,multiplied_ratio=1):
+def read_label(label_filename, labels_list,pcd_ratio=[1,0,0,0]):
     lines = [line.rstrip() for line in open(label_filename)]
-    objects = [Object3d(line, labels_list=labels_list,multiplied_ratio=multiplied_ratio) for line in lines]
+    objects = [Object3d(line, labels_list=labels_list, pcd_ratio=pcd_ratio) for line in lines]
     return objects
